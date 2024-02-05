9f3146f729e6a0507d8269c45aef4089a5e5b66d
seulle <seull@LAPTOP-L9AOK14Q.>
2024-02-06 00:48:12 +0900

    0.1.1 부모자식 불러오기

---
+++ src/components/SalesProduct.vue
@@ -0,0 +1,26 @@
+<script setup>
+import { ref } from 'vue'
+
+async function jsons() {
+    const response = await fetch("http://localhost:3000/posts");
+    const jsonData = await response.json();
+    console.log(jsonData);
+    return jsonData;
+}
+
+const jsonData = ref(null);
+
+// 데이터를 가져와 jsonData 변수에 할당합니다.
+jsons().then(data => {
+    jsonData.value = data;
+});
+</script>
+
+<template>
+<div>
+    <h1>JSON Server 데이터</h1>
+        <li v-for="aaa in jsonData" :key="key">
+            {{ aaa.상품명 }}
+        </li>
+</div>
+</template>
