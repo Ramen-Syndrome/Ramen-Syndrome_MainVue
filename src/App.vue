09cb856cb03688d8e496ac2d581bb5c1ae49d203
seulle <seull@LAPTOP-L9AOK14Q.>
2024-02-06 00:28:24 +0900

    0.1.0

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,26 @@
 <script setup>
-import child from './components/SalesDaily.vue'
-import child from './components/SalesHistory.vue'
-import child from './components/SalesOffice.vue'
-import child from './components/SalesProduct.vue'
+import { ref } from 'vue'
+
+async function jsons() {
+    const response = await fetch("http://localhost:3001/posts");
+    const jsonData = await response.json();
+    console.log(jsonData);
+    return jsonData;
+}
+
+const jsonData = ref(null);
+
+// 데이터를 가져와 jsonData 변수에 할당합니다.
+jsons().then(data => {
+    jsonData.value = data;
+});
 </script>
 
 <template>
-   <child/>
+<div>
+    <h1>JSON Server 데이터</h1>
+        <li v-for="aaa in jsonData" :key="key">
+	    {{ aaa }}
+        </li>
+</div>
 </template>
