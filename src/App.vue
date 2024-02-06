17319c89e4f61807c6df2eebf1d51e55c0f5eaa8
jsmin6330 <jsmin6330@gmail.com>
2024-02-06 16:29:59 +0900

    지점별 전월 매출 실적 화면 추가

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,10 @@
 <script setup>
-import child from './components/SalesDaily.vue'
-import child from './components/SalesHistory.vue'
-import child from './components/SalesOffice.vue'
-import child from './components/SalesProduct.vue'
+//import salesdaily from './components/SalesDaily.vue'
+import saleshistory from './components/SalesHistory.vue'
+//import salesoffice from './components/SalesOffice.vue'
+//import salesproduct from './components/SalesProduct.vue'
 </script>
 
 <template>
-   <child/>
+   <saleshistory/>
 </template>
