ecb23847210b4097cef77ec78991b61990ef1156
qkralsgml78 <qkralsgml78@gmail.com>
2024-02-07 13:53:50 +0900

    판매된 상품 정보 수정

--- src/components/SalesDaily.vue
+++ src/components/SalesDaily.vue
@@ -2,7 +2,7 @@
 import { ref, onMounted } from 'vue';
 
 async function jsons() {
-    const response = await fetch("https://ramen-syndrome-jsonserver.fly.dev/sales");
+    const response = await fetch("http://localhost:3000/sales");
     const jsonData = await response.json();
     return jsonData;
 }
@@ -28,13 +28,19 @@ onMounted(async () => {
             groupedByBranch[key] = { 지점명: sale.지점명, 거래날짜: sale.거래날짜, 상품들: [] };
         }
 
-        groupedByBranch[key].상품들.push({
-            상품명: sale.상품명,
-            판매가: sale.판매가,
-            원가: sale.원가,
-            판매갯수: 1,
-            총매출: sale.판매가
-        });
+        const existingProduct = groupedByBranch[key].상품들.find(product => product.상품명 === sale.상품명);
+
+        if (existingProduct) {
+            existingProduct.총매출 += sale.판매가;
+            existingProduct.판매갯수++;
+        } else {
+            groupedByBranch[key].상품들.push({
+                상품명: sale.상품명,
+                판매가: sale.판매가,
+                총매출: sale.판매가,
+                판매갯수: 1
+            });
+        }
     });
 
     // 중복 제거된 데이터를 배열에 추가합니다.
@@ -48,7 +54,12 @@ onMounted(async () => {
         return {
             지점명: group.지점명,
             거래날짜: group.거래날짜,
-            상품명: group.상품들.map(product => product.상품명).join(', '),
+            상품별정보: group.상품들.map(product => ({
+                상품명: product.상품명,
+                판매가: product.판매가,
+                총매출: product.총매출,
+                판매갯수: product.판매갯수
+            })),
             평균판매가: totalSales.총매출 / totalSales.판매갯수,
             총매출: totalSales.총매출,
             판매갯수: totalSales.판매갯수
@@ -66,67 +77,33 @@ function formatCurrency(value) {
 
 <template>
   <div>
-    <h1 class="center-align">일일 지점별 상품 매출</h1>
-    <h5 class="right-align"> 단위: 원 </h5>
+    <h1>일일 매출 실적</h1>
     <table class="sales-table">
       <thead>
         <tr>
-          <th class="center-align">지점명</th>
-          <th class="center-align">거래날짜</th>
-          <th class="center-align">상품명</th>
-          <th class="center-align">평균 판매가</th>
-          <th class="center-align">총 매출액</th>
-          <th class="center-align">팔린 갯수</th>
+          <th>지점명</th>
+          <th>거래날짜</th>
+          <th>판매된 상품 정보</th>
+          <th>총 매출액</th>
+          <th>팔린 갯수</th>
         </tr>
       </thead>
       <tbody>
         <tr v-for="sale in filteredData" :key="sale.id">
-          <td class="center-align">{{ sale.지점명 }}</td>
-          <td class="center-align">{{ sale.거래날짜 }}</td>
-          <td class="center-align">{{ sale.상품명 }}</td>
-          <td class="right-align">{{ formatCurrency(sale.평균판매가) }}</td>
-          <td class="right-align">{{ formatCurrency(sale.총매출) }}</td>
-          <td class="right-align">{{ sale.판매갯수 }}</td>
+          <td>{{ sale.지점명 }}</td>
+          <td>{{ new Date(sale.거래날짜).toLocaleDateString() }}</td>
+          <td>
+            <table>
+              <tr v-for="productInfo in sale.상품별정보" :key="productInfo.상품명">
+                <td>{{ productInfo.상품명 }}</td>
+                <td>{{ formatCurrency(productInfo.판매가) }}</td>
+              </tr>
+            </table>
+          </td>
+          <td>{{ formatCurrency(sale.총매출) }}</td>
+          <td>{{ sale.판매갯수 }}</td>
         </tr>
       </tbody>
     </table>
   </div>
 </template>
-
-<style>
-
-h1.center-align{
-  text-align: center; /* 중앙 정렬 */
-}
-
-h5.right-align {
-    text-align: right; /* 우측 정렬 */
-}
-
-.sales-table {
-  width: 100%;
-  border-collapse: collapse; /* 테두리 겹침을 방지 */
-}
-
-.sales-table, .sales-table th, .sales-table td {
-  border: 1px solid black; /* 테두리 설정 */
-}
-
-.sales-table th, .sales-table td {
-  padding: 10px; /* 셀 내부 여백 */
-  text-align: left; /* 텍스트 정렬 */
-}
-
-.sales-table thead {
-  /*background-color: #f2f2f2; /* 헤더 배경색 */
-}
-
-.sales-table .center-align { /* 테이블 */
-        text-align: center; /* 중앙 정렬 클래스 */
-}
-
-.sales-table .right-align { /* 테이블 */
-        text-align: right; /* 우측 정렬 클래스 */
-}
-
-</style>
